f58062dfd00b72675bee674d94fe1cff6bf20412
dZallo <danielzallo@gmail.com>
2026-05-17 20:59:42 +0200

    [feat]: uso de la libreria para pasarle un parametro

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+import argparse
 
 
 def main():
     print('Hello World')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("excel")
+    args = parser.parse_args()
+    print(f"Procesando: {args.excel}")
 
 
 if __name__ == '__main__':
